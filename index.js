2d5d9b139dac77adae9fd1c5f72cd9d796d5cc4b
Sharon Kim <skimilk19@gmail.com>
2021-02-27 16:11:04 -0500

    index.js

--- index.js
+++ index.js
@@ -18,19 +18,198 @@ const employeeTeam = [];
 
 function createManager() {
     inquirer
-    .prompt([{
-        name: "name",
-        type: "type",
-        message: "What is the manager's name?",
-        validate: async (input) => {
-            if (input == "" || /\s/.test(input)) {
-                return "Please enter first or last name.";
+        .prompt([{
+            name: "name",
+            type: "type",
+            message: "What is the manager's name?",
+            validate: async (input) => {
+                if (input == "" || /\s/.test(input)) {
+                    return "Please enter first or last name.";
+                }
+                return true;
             }
-            return true; 
-        } 
-    },
-    {
-        
-    }
-])
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the manager's ID number?",
+            validate: async (input) => {
+                if (isNaN(input)) {
+                    return "Please enter a number";
+                }
+                return true;
+            }
+
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is the manager's email?",
+            validate: async (input) => {
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                    return true;
+                }
+                return "Please enter a valid email address.";
+            }
+        },
+        {
+            name: "number",
+            type: "input",
+            message: "What is the manager's phone number?",
+            validate: async (input) => {
+                if (isNaN(input)) {
+                    return "Please enter a number";
+                }
+                return true;
+            }
+        },
+        ])
+        .then(function (response) {
+            console.log(response);
+            const newManager = new Manager(
+                response.name,
+                response.id,
+                response.email,
+                response.number
+            );
+            employeeTeam.push(newManager);
+            fs.createReadStream();
+        });
+}
+
+createManager();
+
+function createTeam() {
+    inquirer
+        .prompt([
+            {
+                name: "addMember",
+                type: "list",
+                message: "Would you like to add a team member",
+                choices: [
+                    "Yes, add a Manager",
+                    "Yes, add an Engineer",
+                    "Yes, add an Intern",
+                    "No my team is complete",
+                ],
+            },
+        ])
+        .then(function (data) {
+            switch (data.addMember) {
+                case "Yes, add a Manager":
+                    createManager();
+                    break;
+            }
+        });
+}
+function createEngineer() {
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the engineer's name?",
+                validate: async (input) => {
+                    if (input == "" || /\s/.test(input)) {
+                        return "Please enter first or last name.";
+                    }
+                    return true;
+                }
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "What is engineer's ID number?",
+                validate: async (input) => {
+                    if (isNaN(input)) {
+                        return "Please enter a number";
+                    }
+                    return true;
+                }
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is the engineer's email?",
+                validate: async (input) => {
+                    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+            },
+            {
+                name: "github",
+                type: "input",
+                message: "What is the engineer's GitHub username?",
+            },
+        ])
+        .then(function (response) {
+            const newEngineer = new Engineer(
+                response.name,
+                response.id,
+                response.email,
+                response.github
+            );
+            employeeTeam.push(newEngineer);
+            createTeam();
+        });
+}
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the intern's name?",
+                validate: async (input) => {
+                    if (input == "" || /\s/.test(input)) {
+                        return "Please enter first or last name.";
+                    }
+                    return true;
+                }
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "What is the intern's ID number?",
+                validate: async (input) => {
+                    if (isNaN(input)) {
+                        return "Please enter a number";
+                    }
+                    return true;
+                }
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is the intern's email?",
+                validate: async (input) => {
+                    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+            },
+            {
+                name: "school",
+                type: "input",
+                message: "What school did the intern go to?",
+            },
+        ])
+        .then(function (response) {
+            const newIntern = new Intern(
+                response.name,
+                response.id,
+                response.email,
+                response.school
+            );
+            employeeTeam.push(newIntern);
+            createTeam();
+        });
+}
+
+function buildTeam() {
+    fs.writeFileSync(outputPath, render(employeeTeam), "utf8");
 }
